d0a913a614aaf31a990da32661a794bdaf7655fa
SethUllman <seth.ullman98@gmail.com>
2024-04-23 14:04:38 -0600

    Add basic navigation

--- src/App.js
+++ src/App.js
@@ -5,19 +5,26 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <ul>
+          <li><a href="#section1">Chess</a></li>
+          <li><a href="#section2">Race Car</a></li>
+          <li><a href="#section3">Wumpus World</a></li>
+        </ul>
       </header>
+      <section id="section1">
+        <h2>Chess</h2>
+        <p>This will be a fully functioning chess game.</p>
+      </section>
+
+      <section id="section2">
+        <h2>Race Car</h2>
+        <p>Visual demonstration on Reinforcement Learning Race Car</p>
+      </section>
+
+      <section id="section3">
+        <h2>Wumpus World</h2>
+        <p>Visual demonstration of Wumpus World logic</p>
+      </section>
     </div>
   );
 }
